74a10b488049f7ba887833d3f45685933f2486ea
BelmarMX <dispersion.mx@gmail.com>
2024-10-14 19:19:33 -0600

    🗃️ Create of Accessors and Mutators for human dates

--- app/Models/BlogCategory.php
+++ app/Models/BlogCategory.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -18,12 +20,34 @@ class BlogCategory extends Model
         ,   'image_rx'
     ];
 
+    protected $appends  = [
+            'human_created_at'
+        ,   'created_dmy'
+    ];
+
     /* ----------------------------------------------------------------------------------------------------------------
      * RELATIONSHIP
     ----------------------------------------------------------------------------------------------------------------- */
     public function blog_articles(): HasMany
     {
         return $this->hasMany(BlogArticle::class);
+    }
+
+    /* ----------------------------------------------------------------------------------------------------------------
+     * MUTATORS AND ACCESSORS
+    ----------------------------------------------------------------------------------------------------------------- */
+    protected function humanCreatedAt(): Attribute
+    {
+        $human = !empty($this->created_at) ? ucfirst($this->created_at->diffForHumans()) : NULL;
+        return Attribute::make(
+            get: fn() => $human
+        );
+    }
+    protected function createdDmy(): Attribute
+    {
+        return Attribute::make(
+            get: fn() => Carbon::parse($this->created_at)->format('d/m/Y H:i')
+        );
     }
 
     /* ----------------------------------------------------------------------------------------------------------------
--- app/Models/Reel.php
+++ app/Models/Reel.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -24,6 +26,11 @@ class Reel extends Model
         ,   'ends_at'
     ];
 
+    protected $appends  = [
+            'human_created_at'
+        ,   'created_dmy'
+    ];
+
     /* ----------------------------------------------------------------------------------------------------------------
      * RELATIONSHIP
     ----------------------------------------------------------------------------------------------------------------- */
@@ -35,6 +42,23 @@ class Reel extends Model
     public function product(): BelongsTo
     {
         return $this->belongsTo(Product::class);
+    }
+
+    /* ----------------------------------------------------------------------------------------------------------------
+     * MUTATORS AND ACCESSORS
+    ----------------------------------------------------------------------------------------------------------------- */
+    protected function humanCreatedAt(): Attribute
+    {
+        $human = !empty($this->created_at) ? ucfirst($this->created_at->diffForHumans()) : NULL;
+        return Attribute::make(
+            get: fn() => $human
+        );
+    }
+    protected function createdDmy(): Attribute
+    {
+        return Attribute::make(
+            get: fn() => Carbon::parse($this->created_at)->format('d/m/Y H:i')
+        );
     }
 
     /* ----------------------------------------------------------------------------------------------------------------
