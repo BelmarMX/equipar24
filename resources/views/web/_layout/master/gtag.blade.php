6c02f97a114199a9147ca8fe09ef58b0fd3925ed
Belmar Alberto <dispersion.mx@gmail.com>
2025-04-04 19:41:17 -0600

    📈 Se agrega el Google Tag Manager

--- resources/views/web/_layout/master/gtag.blade.php
+++ resources/views/web/_layout/master/gtag.blade.php
@@ -1,12 +1,7 @@
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-WNHVY5P874"></script>
-<script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'G-WNHVY5P874');
-</script>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-TV3GXHLC');</script>
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TV3GXHLC" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 
 <!-- Meta Pixel Code -->
 <script>
